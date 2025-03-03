c2ee05e3bcb47cb35aca260cac5420a6cb8bb8c5
QuiGon189 <leshka189@gmail.com>
2025-03-04 00:08:10 +0300

    add docerfile, compose.yml

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=scavanger.py
-FLASK_DEBUG=1
--- scavenger/config.py
+++ scavenger/config.py
@@ -2,3 +2,4 @@ import os
 
 class Config():
     SECRET_KEY = os.environ.get('SECTRE_KEY') or 'ghjgecr123'
+    DEBUG = os.environ.get('DEBUG') or False
