6113b3f499f40aa71b0830c58fb94bfb9e71d62d
QuiGon189 <leshka189@gmail.com>
2025-03-04 12:54:36 +0300

    rename Dockerfile

--- scavenger/config.py
+++ scavenger/config.py
@@ -1,5 +1,4 @@
 import os
 
 class Config():
-    SECRET_KEY = os.environ.get('SECTRE_KEY') or 'ghjgecr123'
-    DEBUG = os.environ.get('DEBUG') or False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ghjgecr123'
