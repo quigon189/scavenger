d87015f0664a79a27d971391b6630e13e7656168
QuiGon189 <leshka189@gmail.com>
2026-01-26 17:03:13 +0300

    add handlers

--- services/auth-service/internal/handlers/auth_handler.go
+++ services/auth-service/internal/handlers/auth_handler.go
@@ -28,6 +28,8 @@ func (h *AuthHandler) RegisterRoutes(router *http.ServeMux) {
 
 	// require auth
 	router.HandleFunc("GET /logout", h.RequireAuth(h.Logout))
+	router.HandleFunc("POST /validate", h.RequireAuth(h.ValidateSession))
+	router.HandleFunc("POST /change_password", h.RequireAuth(h.ChangePassword))
 }
 
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
@@ -102,6 +104,59 @@ func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"message": "Logged out successfully"})
+}
+
+func (h *AuthHandler) ValidateSession(w http.ResponseWriter, r *http.Request) {
+	sessionID, ok := r.Context().Value("session_id").(string)
+	if !ok {
+		http.Error(w, "session not found", http.StatusBadRequest)
+		log.Printf("session not found")
+		return
+	}
+
+	session, ok := r.Context().Value("session").(*models.UserSession)
+	if !ok {
+		http.Error(w, "session not found", http.StatusBadRequest)
+		log.Printf("session not found")
+		return
+	}
+
+	response := &models.AuthResponse{
+		User:      session.User,
+		SessionID: sessionID,
+		ExpiresAt: session.ExpiresAt,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *AuthHandler) ChangePassword(w http.ResponseWriter, r *http.Request) {
+	session, ok := r.Context().Value("session").(*models.UserSession)
+	if !ok {
+		http.Error(w, "session not found", http.StatusBadRequest)
+		log.Printf("session not found")
+		return
+	}
+
+	var req struct {
+		OldPassword string `json:"old_password"`
+		NewPassword string `json:"new_password"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		log.Printf("invalid request body: %v", err)
+		return
+	}
+
+	if err := h.authService.ChangePassword(r.Context(), session.User.ID, req.OldPassword, req.NewPassword); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		log.Printf("bad request: %v", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Password changed successfully"})
 }
 
 func (h *AuthHandler) RequireAuth(next http.HandlerFunc) http.HandlerFunc {
